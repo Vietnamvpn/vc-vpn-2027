f7b7d96f6221cc26a7b786f2795f8dd09e800dcd
vietnamvpn <kangst24h@gmail.com>
2026-09-08 14:06:32 +0800

    3

--- app/Controllers/Admin/ReferralController.php
+++ app/Controllers/Admin/ReferralController.php
@@ -3,20 +3,27 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\ReferralCommission;
 
 class ReferralController extends BaseController
 {
+    private ReferralCommission $referralModel;
+
     public function __construct()
     {
         if (!isset($_SESSION['user_id']) || ($_SESSION['role'] ?? '') !== 'admin') {
             $this->redirect('/auth/login');
         }
+        $this->referralModel = new ReferralCommission();
     }
 
     public function index(): void
     {
+        $commissions = $this->referralModel->allWithDetails();
+
         $this->render('admin.referrals.index', [
-            'activeMenu' => 'referrals'
+            'activeMenu'  => 'referrals',
+            'commissions' => $commissions
         ]);
     }
 }
